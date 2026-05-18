f6dc44249a7a113aa9bd46780b996e46b5f8e7bb
Yumi Yukimura <me.cafebabe@gmail.com>
2026-05-18 14:45:10 +0800

    snowcastle: Build and load linux-apfs-rw
    * https://github.com/linux-apfs/linux-apfs-rw
    
    Change-Id: Idfe80184107880334673047d467ceb17120797ad

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,9 +76,25 @@ BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD := \
     $(strip $(shell cat $(DEVICE_PATH)/configs/modprobe/modules.load.basic))
 BOARD_VENDOR_KERNEL_MODULES_LOAD := \
     $(BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD :=
+BOOT_KERNEL_MODULES :=
 RECOVERY_KERNEL_MODULES := \
     $(BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD)
 TARGET_AUTO_COLLECT_KERNEL_MODULE_DEPS := true
+
+ifeq ($(SNOWCASTLE_PARTITION_SCHEME),apfs)
+BOARD_RECOVERY_RAMDISK_KERNEL_MODULES_LOAD += \
+    $(strip $(shell cat $(DEVICE_PATH)/configs/modprobe/modules.load.apfs))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD += \
+    $(strip $(shell cat $(DEVICE_PATH)/configs/modprobe/modules.load.apfs))
+BOOT_KERNEL_MODULES += \
+    $(strip $(shell cat $(DEVICE_PATH)/configs/modprobe/modules.load.apfs))
+RECOVERY_KERNEL_MODULES += \
+    $(strip $(shell cat $(DEVICE_PATH)/configs/modprobe/modules.load.apfs))
+
+TARGET_KERNEL_EXT_MODULE_ROOT := $(TARGET_KERNEL_SOURCE)-modules
+TARGET_KERNEL_EXT_MODULES := linux-apfs-rw
+endif
 
 # OTA
 ifneq ($(SNOWCASTLE_PARTITION_SCHEME),normal)
