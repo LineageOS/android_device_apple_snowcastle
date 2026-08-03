e4f11bf38bd94fa51fc8ced2568b848ba46d2c8f
Yumi Yukimura <me.cafebabe@gmail.com>
2026-08-04 01:36:22 +0800

    snowcastle: Add `snowcastle_legacy` product
    Change-Id: I9c620e732357cb4b35c16797ef025fda9c411560

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,7 +4,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_snowcastle.mk
+    $(LOCAL_DIR)/lineage_snowcastle.mk \
+    $(LOCAL_DIR)/snowcastle_legacy/lineage_snowcastle_legacy.mk
 
 $(foreach build_type, user userdebug eng, \
-    $(eval COMMON_LUNCH_CHOICES += lineage_snowcastle-$(build_type)))
+    $(eval COMMON_LUNCH_CHOICES += lineage_snowcastle-$(build_type)) \
+    $(eval COMMON_LUNCH_CHOICES += lineage_snowcastle_legacy-$(build_type)))
---
+++ snowcastle_legacy/BoardConfig.mk
@@ -0,0 +1,7 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from upper BoardConfig.mk
+include device/apple/snowcastle/BoardConfig.mk
