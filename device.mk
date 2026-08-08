718d51eb3c45e365d0524a56260bc7b3831566d8
Yumi Yukimura <me.cafebabe@gmail.com>
2026-08-08 23:25:29 +0800

    snowcastle: Decouple `generic_init` with `SNOWCASTLE_PARTITION_SCHEME`
    Change-Id: I381dd44f6800370689c09eb7e6553b36b7256187

--- device.mk
+++ device.mk
@@ -7,7 +7,12 @@ DEVICE_PATH := device/apple/snowcastle
 
 # Defaults
 SNOWCASTLE_PARTITION_SCHEME ?= normal
+SNOWCASTLE_USE_GENERIC_INIT ?= false
 $(warning Using $(SNOWCASTLE_PARTITION_SCHEME) partition scheme)
+
+ifeq ($(SNOWCASTLE_PARTITION_SCHEME),normal)
+SNOWCASTLE_USE_GENERIC_INIT := true
+endif
 
 # Inherit from mainline/common
 TARGET_HEALTH_HAL := default-aidl
@@ -37,6 +42,7 @@ endif
 ifeq ($(PRODUCT_IS_GO),true)
 $(call inherit-product, frameworks/native/build/phone-hdpi-512-dalvik-heap.mk)
 else ifeq ($(SNOWCASTLE_PARTITION_SCHEME),apfs)
+# APFS stores userdata in RAM, so let's save some RAM
 $(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
 else
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
