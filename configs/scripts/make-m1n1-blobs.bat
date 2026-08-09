7877a0792455172aa9bcdd509df3b1e63e2127ee
Yumi Yukimura <me.cafebabe@gmail.com>
2026-08-09 14:59:08 +0800

    snowcastle: Add scripts to make m1n1 blobs to snowcastle package
    Change-Id: Ic043aaa28f6771e87cad4cbf243b093c2d9833b8

---
+++ configs/scripts/make-m1n1-blobs.bat
@@ -0,0 +1,15 @@
+@echo off
+setlocal enableextensions
+
+set "SYSCFG=m1n1-syscfg.payload"
+
+if not exist "%SYSCFG%" (
+    echo Warning: %SYSCFG% does not exist
+    copy /b m1n1.bin + m1n1-vars-boot.txt + dtb.img + kernel + ramdisk.img m1n1-boot.bin
+    copy /b m1n1.bin + m1n1-vars-boot.txt + dtb.img + kernel + ramdisk-recovery.img m1n1-recovery.bin
+) else (
+    copy /b m1n1.bin + m1n1-vars-boot.txt + dtb.img + "%SYSCFG%" + kernel + ramdisk.img m1n1-boot.bin
+    copy /b m1n1.bin + m1n1-vars-boot.txt + dtb.img + "%SYSCFG%" + kernel + ramdisk-recovery.img m1n1-recovery.bin
+)
+
+pause
