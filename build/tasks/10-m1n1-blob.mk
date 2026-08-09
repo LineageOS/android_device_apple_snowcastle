991c20078cbbf8b3b8e6dc8c77f4343b8efee3dc
Yumi Yukimura <me.cafebabe@gmail.com>
2026-08-09 13:45:45 +0800

    snowcastle: build: Add build target for snowcastle package
    Change-Id: Ie52e4c68903da0f43a321e1be5ef09733bd34b58

--- build/tasks/10-m1n1-blob.mk
+++ build/tasks/10-m1n1-blob.mk
@@ -17,12 +17,16 @@ $(warning $(M1N1_SYSCFG_PAYLOAD_PATH) is missing)
 M1N1_SYSCFG_PAYLOAD_PATH :=
 endif
 
+INSTALLED_M1N1_TARGET := $(PRODUCT_OUT)/m1n1.bin
+$(INSTALLED_M1N1_TARGET): $(M1N1_BIN_PATH)
+	$(transform-prebuilt-to-target)
+
 # $(1): variant name
 # $(2): ramdisk image filename
 define define-m1n1-blob
 INSTALLED_M1N1_$(call to-upper,$(1))_TARGET := $(PRODUCT_OUT)/m1n1-$(1).bin
 INSTALLED_M1N1_$(call to-upper,$(1))_TARGET_DEPS := \
-	$$(M1N1_BIN_PATH) \
+	$$(INSTALLED_M1N1_TARGET) \
 	$$(INSTALLED_M1N1_VARS_$(call to-upper,$(1))_TXT_TARGET) \
 	$$(INSTALLED_DTBIMAGE_TARGET) \
 	$$(M1N1_SYSCFG_PAYLOAD_PATH) \
---
+++ build/tasks/90-snow.mk
@@ -0,0 +1,44 @@
+#
+# SPDX-FileCopyrightText: The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+ifeq ($(USES_DEVICE_APPLE_SNOWCASTLE),true)
+
+LOCAL_INCLUDE_IN_PRODUCT_OUT += \
+    dtb.img \
+    kernel \
+    m1n1-vars-boot.txt \
+    m1n1-vars-recovery.txt \
+    ramdisk.img \
+    ramdisk-recovery.img \
+    system.img \
+    vendor.img
+
+ifeq ($(BOARD_USES_VENDOR_DLKMIMAGE),true)
+LOCAL_INCLUDE_IN_PRODUCT_OUT += \
+    vendor_dlkm.img
+endif
+
+ifneq ($(INSTALLED_M1N1_TARGET),)
+LOCAL_INCLUDE_IN_PRODUCT_OUT += \
+    m1n1.bin
+endif
+
+ifneq ($(LINEAGE_BUILD),)
+INSTALLED_SNOW_TARGET := $(PRODUCT_OUT)/snowcastle-LineageOS-$(LINEAGE_VERSION).zip
+else
+INSTALLED_SNOW_TARGET ?= $(PRODUCT_OUT)/snowcastle-Android-$(PLATFORM_VERSION_LAST_STABLE)-$(BUILD_ID)-$(LOCAL_BUILD_DATE)-$(TARGET_PRODUCT).zip
+endif
+INSTALLED_SNOW_TARGET_DEPS_PRODUCT_OUT := $(addprefix $(PRODUCT_OUT)/,$(LOCAL_INCLUDE_IN_PRODUCT_OUT))
+LOCAL_SOONG_ZIP_EXEC := $(HOST_OUT_EXECUTABLES)/soong_zip
+
+$(INSTALLED_SNOW_TARGET): $(INSTALLED_SNOW_TARGET_DEPS_PRODUCT_OUT) $(LOCAL_SOONG_ZIP_EXEC)
+	$(call pretty,"Project Snowcastle output package: $@")
+	$(LOCAL_SOONG_ZIP_EXEC) -o $@ -C $(PRODUCT_OUT) $(foreach f,$(INSTALLED_SNOW_TARGET_DEPS_PRODUCT_OUT),-f $(f))
+
+.PHONY: snow snow-deps
+snow: $(INSTALLED_SNOW_TARGET)
+snow-deps: $(INSTALLED_SNOW_TARGET_DEPS_PRODUCT_OUT)
+
+endif # USES_DEVICE_APPLE_SNOWCASTLE
